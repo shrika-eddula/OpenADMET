11cefb73d7cb01bf9bc108f7bfcfa48d00abdf89
shrika <shrika@login008.inband>
2025-11-08 19:40:44 -0500

    training scripts for combined

---
+++ run_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -p mit_normal_gpu
+#SBATCH --job-name=chemeleon
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=15
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+#SBATCH --output=slurm-%j-%x.out
+
+# Load your shell environment to activate your uv environment
+# source chemprop_env/bin/activate
+conda activate OpenADMET
+
+# Run command or script
+python models/CheMeleon/training_combined.py models/CheMeleon/out
